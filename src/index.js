8af8e16428bec7eddc2915ce5b90da147b4a1a6c
Dinesh <dineshmj82@gmail.com>
2020-12-17 10:13:09 +0530

    add bootstrap and w3.css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'jquery'
+import 'popper.js'
+import 'bootstrap/dist/js/bootstrap.min.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './w3.css'
 
 ReactDOM.render(
   <React.StrictMode>
