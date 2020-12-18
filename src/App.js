d7b713fe8ee5aecd1d0ca0e37c1df145c97fecac
Dinesh <dineshmj82@gmail.com>
2020-12-18 14:44:48 +0530

    add provider in app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
-
+import Router from './router'
+import {Provider} from 'react-redux'
+import store from './redux/Store'
 function App() {
   return (
     <div>
-      
+      <Provider store={store}>
+        <Router />
+      </Provider>      
     </div>
   );
 }
