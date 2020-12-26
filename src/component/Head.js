5f6ce20a2036ef7dbc023361f2d4656b28dc3b53
Dinesh <dineshmj82@gmail.com>
2020-12-26 20:40:10 +0530

    head

--- src/component/Head.js
+++ src/component/Head.js
@@ -1,11 +1,28 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { Avatar } from '@material-ui/core'
+import { Link } from 'react-router-dom'
 
 function Head() {
+    const login = useSelector(state => state.login)
     return (
-        <div className="w3-container w3-teal">
-            <p className="w3-left pt-2">MyAcc</p>
+        <div className="w3-container w3-teal" id="head">
+            <div id="headtitle">
+                <p className="w3-left">MyAcc</p>
+            </div>
+
+            <div id="headprofile">
+                <p className="pt-3 pr-2">{login.username}</p>
+                <Link to="/profile">
+                    <Avatar />
+                </Link>
+            </div>
+
+
+
+
         </div>
     )
 }
- 
+
 export default Head
--- src/component/reason/ReasonForm.js
+++ src/component/reason/ReasonForm.js
@@ -7,6 +7,7 @@ import { useSelector } from 'react-redux'
 function ReasonForm({ initialValues, CancelEdit, Reload, Reset, update }) {
 
     const title = useSelector(state => state.titleid)
+    
 
     
 
